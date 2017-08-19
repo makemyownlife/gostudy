826345beee36082790266599e7e78a7162ae3e1c
zhangyong <zhangyong7120180@163.com>
2017-08-19 17:43:50 +0800

    本地上传

--- src/gosftp/gosftp.go
+++ src/gosftp/gosftp.go
@@ -4,53 +4,93 @@ import (
 	"fmt"
 	"time"
 	"log"
-	"os"
 	"golang.org/x/crypto/ssh"
+	"github.com/pkg/sftp"
+	"net"
+	"os"
+	"path"
 )
 
 func main() {
 	fmt.Println("hello ,world")
-	duration := time.Duration(10) * time.Second
-	time.Sleep(duration)
 
-	session, err := connect("root", "xxxxx", "127.0.0.1", 22)
+	sftpClient, err := connect("root", "123456a?", "192.168.1.205", 22)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer session.Close()
+	defer sftpClient.Close()
 
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-	session.Run("ls /; ls /abc")
+	// 用来测试的本地文件路径 和 远程机器上的文件夹
+	var localFilePath = "D:\\server\\lib"
+	var remoteDir = "/opt"
+	srcFile, err := os.Open(localFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer srcFile.Close()
+
+	var remoteFileName = path.Base(localFilePath)
+	dstFile, err := sftpClient.Create(path.Join(remoteDir, remoteFileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dstFile.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, _ := srcFile.Read(buf)
+		if n == 0 {
+			break
+		}
+		dstFile.Write(buf)
+	}
+
+	fmt.Println("copy file to remote server finished!")
+
+	//睡一会
+	sleepForAwhile(10)
+
+	sleepForAwhile(2)
 }
 
-func connect(user, password, host string, port int) (*ssh.Session, error) {
+func connect(user, password, host string, port int) (*sftp.Client, error) {
 	var (
-		auth  []ssh.AuthMethod
-		addr  string
+		auth         []ssh.AuthMethod
+		addr         string
 		clientConfig *ssh.ClientConfig
-		client *ssh.Client
-		session *ssh.Session
-		err  error
+		sshClient    *ssh.Client
+		sftpClient   *sftp.Client
+		err          error
 	)
 	// get auth method
 	auth = make([]ssh.AuthMethod, 0)
 	auth = append(auth, ssh.Password(password))
 
 	clientConfig = &ssh.ClientConfig{
-		User: user,
-		Auth: auth,
+		User:    user,
+		Auth:    auth,
 		Timeout: 30 * time.Second,
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
 	}
+
 	// connet to ssh
 	addr = fmt.Sprintf("%s:%d", host, port)
 
-	if client, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
+	if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
 		return nil, err
 	}
-	// create session
-	if session, err = client.NewSession(); err != nil {
+
+	// create sftp client
+	if sftpClient, err = sftp.NewClient(sshClient); err != nil {
 		return nil, err
 	}
-	return session, nil
+
+	return sftpClient, nil
+}
+
+func sleepForAwhile(sleepTime int) {
+	duration := time.Duration(sleepTime) * time.Second
+	time.Sleep(duration)
 }
