45dce9481362f8ecba574cdb17cb6eeea66c4e95
zhangyong <zhangyong7120180@163.com>
2017-10-26 17:38:43 +0800

    cms api 相关的内容

--- src/cms/cmd/cmsMain.go
+++ src/cms/cmd/cmsMain.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"cms"
 )
 
 func main() {
 	fmt.Println("begin to cms data 😆");
+	cms.GetHomeData();
 }
 
---
+++ src/cms/cmsHome.go
@@ -0,0 +1,24 @@
+package cms
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
+
+func GetHomeData() {
+	cmsUrl := "http://xxcmsapi.biyao.com/homepage/getData?pageSize=10&curPageNumber=1";
+	resp, err := http.Get(cmsUrl)
+	if err != nil {
+		// handle error
+		fmt.Println("can't get data from cmsapi")
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+	fmt.Println(string(body))
+}
