7d5726ec2e0a8c28eed48dad26f586ba171d2cf4
zhangyong <zhangyong7120180@163.com>
2022-05-31 11:32:53 +0800

    [新增代码] 非dubbo版本

--- src/main.go
+++ src/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "go-study/src/template"
+import (
+	"github.com/pelletier/go-toml"
+	"go-study/src/template"
+	"log"
+	"os"
+)
 
 func main() {
-	template.CreateBootMavenProject()
+	execDirAbsPath, _ := os.Getwd()
+	log.Println("执行程序所在目录的绝对路径　　　　　　　:", execDirAbsPath)
+	config, err := toml.LoadFile(execDirAbsPath + "/config/boot/config.toml")
+	if err != nil {
+		log.Println("Error ", err.Error())
+	} else {
+		isDubbo := config.Get("maven.isDubbo").(string)
+		log.Println("是否dubbo项目：" + isDubbo)
+		if isDubbo == "1" {
+			template.CreateDubboMavenProject()
+		} else {
+			template.CreateBootMavenProject()
+		}
+	}
 }
--- src/template/bootTemplate.go
+++ src/template/bootTemplate.go
@@ -16,7 +16,7 @@ type Inventory2 struct {
 	BasePackage      string
 }
 
-func CreateBootMavenProject() {
+func CreateDubboMavenProject() {
 	execDirAbsPath, _ := os.Getwd()
 	log.Println("执行程序所在目录的绝对路径　　　　　　　:", execDirAbsPath)
 	config, err := toml.LoadFile(execDirAbsPath + "/config/boot/config.toml")
@@ -317,6 +317,241 @@ func CreateBootMavenProject() {
 		createSrcDir(serverMainPath + separator + "assembly" + separator + "bin")
 		renderOtherFile("config/boot/static/server/assembly/binTemp/start.sh.temp", &pomObject, serverMainPath+separator+"assembly"+separator+"bin"+separator+"start.sh")
 		renderOtherFile("config/boot/static/server/assembly/binTemp/stop.sh.temp", &pomObject, serverMainPath+separator+"assembly"+separator+"bin"+separator+"stop.sh")
+
+		//===================================================================================================创建demo模块 ===================================================================================================
+
+	}
+
+}
+
+func CreateBootMavenProject() {
+	execDirAbsPath, _ := os.Getwd()
+	log.Println("执行程序所在目录的绝对路径　　　　　　　:", execDirAbsPath)
+	config, err := toml.LoadFile(execDirAbsPath + "/config/boot/config.toml")
+	if err != nil {
+		log.Println("Error ", err.Error())
+	} else {
+		path := config.Get("maven.path").(string)
+		projectName := config.Get("maven.projectName").(string)
+		moduleNamePrefix := config.Get("maven.moduleNamePrefix").(string)
+		groupId := config.Get("maven.groupId").(string)
+		basePackage := config.Get("maven.basePackage").(string)
+
+		log.Println("项目根路径：" + path)
+		log.Println("项目名:" + projectName)
+		log.Println("文件分隔符:" + string(os.PathSeparator))
+		log.Println("模块名前缀：" + moduleNamePrefix)
+		log.Println("base包目录：" + basePackage)
+
+		projectPath := path + string(os.PathSeparator) + projectName
+		log.Println("trying to create ProjectPath:    " + projectPath)
+
+		//开始创建工程目录
+		err := os.MkdirAll(projectPath, 0777)
+		if err != nil {
+			log.Printf("%s", err)
+			return
+		}
+		log.Println("Create Direcry OK!")
+
+		//创建pom文件
+		pomObject := Inventory2{groupId, projectName, moduleNamePrefix, basePackage}
+		//创建pom文件目录
+		pomStr := readStaticFile("config/boot/static/pom.xml")
+		pomTmp, err := template.New("pom").Parse(pomStr) //建立一个模板
+		//将struct与模板合成，合成结果放到os.Stdout里
+		var pomPath = projectPath + string(os.PathSeparator) + "pom.xml"
+		log.Println("create pomfile start pomPath:" + pomPath)
+		pomWriter, err3 := os.Create(pomPath) //创建文件
+		err = pomTmp.Execute(pomWriter, pomObject)
+		if err != nil {
+			panic(err)
+		}
+		if err3 != nil {
+			panic(err)
+		}
+		defer pomWriter.Close()
+		log.Println("create pomfile success")
+
+		var separator = string(os.PathSeparator)
+
+		//复制 pom gitignore文件到目的目录
+		ignoreFilePath := []string{projectPath, ".gitignore"}
+		copyStaticFileToTarget("config/boot/static/.gitignore", strings.Join(ignoreFilePath, string(os.PathSeparator)))
+
+		createSrcDir(projectPath + string(os.PathSeparator) + "lib")
+		copyStaticFileToTarget("config/boot/static/lib/training-framework-redis-1.0-SNAPSHOT.jar", projectPath+string(os.PathSeparator)+"lib/training-framework-redis-1.0-SNAPSHOT.jar")
+		//===================================================================================================创建common 模块 ===================================================================================================
+		var commonModule = moduleNamePrefix + "-common"
+		var commonPath = projectPath + string(os.PathSeparator) + commonModule
+		os.MkdirAll(commonPath, 0777)
+
+		var commonSrcPath = commonPath + string(os.PathSeparator) + "src"
+		log.Println("commonSrcPath:   " + commonSrcPath)
+		createSrcDir(commonSrcPath)
+
+		arr := strings.Split(basePackage, ".")
+		packageStr := strings.Join(arr, string(os.PathSeparator))
+		log.Println("包目录：    " + packageStr)
+
+		var commonMainPath = commonSrcPath + separator + "main"
+		var commonJavaPath = commonMainPath + separator + "java"
+		var commonResPath = commonMainPath + separator + "resources"
+		var commonTestPath = commonSrcPath + separator + "test"
+		var commonTestJavaPath = commonTestPath + separator + "java"
+
+		createSrcDir(commonJavaPath)
+		createSrcDir(commonResPath)
+		createSrcDir(commonTestPath)
+		createSrcDir(commonTestJavaPath)
+
+		commonClasspath := commonJavaPath + string(os.PathSeparator) + packageStr + string(os.PathSeparator) + "common"
+		createSrcDir(commonClasspath)
+
+		var commonPackageInfo = "package " + basePackage + ".common;"
+		ioutil.WriteFile(commonClasspath+separator+"package-info.java", []byte(string(commonPackageInfo)), 0777)
+
+		renderPomFile("config/boot/static/common/pom.xml", &pomObject, commonPath)
+
+		//===================================================================================================创建domain模块 ===================================================================================================
+		var domainModule = moduleNamePrefix + "-domain"
+		var domainPath = projectPath + string(os.PathSeparator) + domainModule
+		os.MkdirAll(domainPath, 0777)
+
+		var domainSrcPath = domainPath + string(os.PathSeparator) + "src"
+		log.Println("domainSrcPath:   " + domainSrcPath)
+		createSrcDir(domainSrcPath)
+
+		var domainMainPath = domainSrcPath + separator + "main"
+		var domainJavaPath = domainMainPath + separator + "java"
+		var domainResPath = domainMainPath + separator + "resources"
+		var domainTestPath = domainSrcPath + separator + "test"
+		var domainTestJavaPath = domainTestPath + separator + "java"
+
+		createSrcDir(domainJavaPath)
+		createSrcDir(domainResPath)
+		createSrcDir(domainTestPath)
+		createSrcDir(domainTestJavaPath)
+
+		domainClasspath := domainJavaPath + string(os.PathSeparator) + packageStr + string(os.PathSeparator) + "domain"
+		createSrcDir(domainClasspath)
+
+		var domainPackageInfo = "package " + basePackage + ".domain;"
+		ioutil.WriteFile(domainClasspath+separator+"package-info.java", []byte(string(domainPackageInfo)), 0777)
+
+		renderPomFile("config/boot/static/domain/pom.xml", &pomObject, domainPath)
+		createSrcDir(domainClasspath + separator + "mapper")
+
+		renderOtherFile(
+			"config/boot/static/domain/UserMapper.java",
+			&pomObject,
+			domainClasspath+separator+"mapper"+separator+"UserMapper.java")
+
+		createSrcDir(domainClasspath + separator + "po")
+
+		renderOtherFile(
+			"config/boot/static/domain/User.java",
+			&pomObject,
+			domainClasspath+separator+"po"+separator+"User.java")
+
+		mapperDir := domainResPath + separator + "mapper"
+		createSrcDir(mapperDir)
+
+		renderOtherFile(
+			"config/boot/static/domain/UserMapper.xml",
+			&pomObject,
+			mapperDir+separator+"UserMapper.xml")
+
+		//===================================================================================================创建service模块 ===================================================================================================
+
+		var serviceModule = moduleNamePrefix + "-service"
+		var servicePath = projectPath + string(os.PathSeparator) + serviceModule
+		os.MkdirAll(servicePath, 0777)
+
+		var serviceSrcPath = servicePath + string(os.PathSeparator) + "src"
+		log.Println("serviceSrcPath:   " + serviceSrcPath)
+		createSrcDir(serviceSrcPath)
+
+		var serviceMainPath = serviceSrcPath + separator + "main"
+		var serviceJavaPath = serviceMainPath + separator + "java"
+		var serviceResPath = serviceMainPath + separator + "resources"
+		var serviceTestPath = serviceSrcPath + separator + "test"
+		var serviceTestJavaPath = serviceTestPath + separator + "java"
+
+		createSrcDir(serviceJavaPath)
+		createSrcDir(serviceResPath)
+		createSrcDir(serviceTestPath)
+		createSrcDir(serviceTestJavaPath)
+
+		serviceClasspath := serviceJavaPath + string(os.PathSeparator) + packageStr + string(os.PathSeparator) + "service"
+		createSrcDir(serviceClasspath)
+
+		var servicePackageInfo = "package " + basePackage + ".service;"
+		ioutil.WriteFile(serviceClasspath+separator+"package-info.java", []byte(string(servicePackageInfo)), 0777)
+
+		renderPomFile("config/boot/static/service/pom.xml", &pomObject, servicePath)
+
+		renderOtherFile(
+			"config/boot/static/service/UserService.java",
+			&pomObject,
+			serviceClasspath+separator+"UserService.java")
+
+		//===================================================================================================创建server模块 ===================================================================================================
+		var serverModule = moduleNamePrefix + "-server"
+		var serverPath = projectPath + string(os.PathSeparator) + serverModule
+		os.MkdirAll(serverPath, 0777)
+
+		var serverSrcPath = serverPath + string(os.PathSeparator) + "src"
+		log.Println("serverSrcPath:   " + serverSrcPath)
+		createSrcDir(serverSrcPath)
+
+		var serverMainPath = serverSrcPath + separator + "main"
+		var serverJavaPath = serverMainPath + separator + "java"
+		var serverResPath = serverMainPath + separator + "resources"
+		var serverTestPath = serverSrcPath + separator + "test"
+		var serverTestJavaPath = serverTestPath + separator + "java"
+
+		createSrcDir(serverJavaPath)
+		createSrcDir(serverResPath)
+		createSrcDir(serverTestPath)
+		createSrcDir(serverTestJavaPath)
+
+		serverClasspath := serverJavaPath + string(os.PathSeparator) + packageStr + string(os.PathSeparator) + "server"
+		createSrcDir(serverClasspath)
+
+		renderPomFile("config/boot/static/server-boot/pom.xml", &pomObject, serverPath)
+
+		renderOtherFile(
+			"config/boot/static/server-boot/MainApplication.java",
+			&pomObject,
+			serverClasspath+separator+"MainApplication.java")
+
+		renderOtherFile(
+			"config/boot/static/server-boot/application.yml",
+			&pomObject,
+			serverResPath+separator+"application.yml")
+
+		renderOtherFile(
+			"config/boot/static/server-boot/application-prod.yml",
+			&pomObject,
+			serverResPath+separator+"application-prod.yml")
+
+		renderOtherFile(
+			"config/boot/static/server-boot/application-test.yml",
+			&pomObject,
+			serverResPath+separator+"application-test.yml")
+
+		createSrcDir(serverResPath + separator + "logger")
+
+		renderOtherFile("config/boot/static/server-boot/logback-prod.xml", &pomObject, serverResPath+separator+"logger"+separator+"logback-prod.xml")
+		renderOtherFile("config/boot/static/server-boot/logback-test.xml", &pomObject, serverResPath+separator+"logger"+separator+"logback-test.xml")
+
+		// 添加打包相关内容
+		createSrcDir(serverMainPath + separator + "assembly")
+		renderOtherFile("config/boot/static/server-boot/assembly/assembly.xml", &pomObject, serverMainPath+separator+"assembly"+separator+"assembly.xml")
+		createSrcDir(serverMainPath + separator + "assembly" + separator + "bin")
+		renderOtherFile("config/boot/static/server-boot/assembly/binTemp/start.sh.temp", &pomObject, serverMainPath+separator+"assembly"+separator+"bin"+separator+"start.sh")
+		renderOtherFile("config/boot/static/server-boot/assembly/binTemp/stop.sh.temp", &pomObject, serverMainPath+separator+"assembly"+separator+"bin"+separator+"stop.sh")
 
 		//===================================================================================================创建demo模块 ===================================================================================================
 
